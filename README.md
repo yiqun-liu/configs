a335d046b62c7e87c49d699ab39d1fae9a98dce1
Yiqun Liu <yiqun.liu.dev@outlook.com>
2023-09-10 01:02:53 +0800

    initial commit: management scripts and README

---
+++ README.md
@@ -0,0 +1,39 @@
+# Configurations
+
+A repo of Yiqun's personal configuration files. The goal is to make my configurable software behaves
+consistently on all machines. The `manage.sh` script could gather (for persisting) and distribute
+(for deployment) the configuration files from / to their working path.
+
+The configs mostly come from the official guide and demand-driven searching. Some ideas are taken
+from projects like [lazyVim](https://www.lazyvim.org/).
+
+## Usage
+
+```bash
+# create a working copy
+cp tracked-configs.example tracked-configs
+
+# custmoize the file when necessary
+nvim tracked-configs
+
+# to persist local changes
+./manage.sh collect
+# git push origin main
+
+# to put the configs into effect
+# git pull origin main
+./manage.sh deploy
+```
+
+Remember that the configurations could have external dependencies. Take `nvim` as an example, we
+have to manually install the package manager (then download all packages) and the LSP servers.
+
+Sometimes there are compiled files or downloaded files which we would not like to persist in this
+repo. We filter them out with the top level `.gitignore`.
+
+## TODO
+
+- Support single file config like `.gitconfig`, `.vimrc`.
+- Include a `.vimrc` file as a reasonble fallback.
+- nvim: consider how to manage project-local settings (indentations).
+
---
+++ manage.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+repo_dir=`dirname $0`
+repo_dir=`realpath $repo_dir`
+order=$1
+
+# check_parameters
+if [[ $# -ne 1 || ( $order != "collect" && $order != "deploy" ) ]]
+then
+	echo "Expect one command argument: [collect|deploy]"
+	exit -1
+fi
+
+track_config_path=$repo_dir/tracked_configs
+if ! test -e $track_config_path
+then
+	echo "Tracked configs not found. Please put configurations at path $track_config_path."
+	echo "You could customize your settings based on $repo_dir/traced_configs.example."
+	exit -1
+fi
+
+set -e
+
+source $track_config_path
+
+echo start $order:
+for mapping in "${mappings[@]}"
+do
+	# seperate fields with the default IFS (whitespace)
+	read config_name config_dir <<< $mapping
+	repo_config_dir=$repo_dir/$config_name
+
+	if test $order == "collect"
+	then
+		echo "$config_name: $config_dir --> $repo_config_dir"
+		rm -rf $repo_config_dir
+		cp -r $config_dir $repo_config_dir
+	elif test $order == "deploy"
+	then
+		read -p "deploy: $config_name --> $config_dir? (y/n)" choice
+		while test $choice != 'y' && $choice != 'n'
+		do
+			echo "Invalid choice: enter 'y' to confirm or 'n' to skip."
+			read -p "deploy: $config_name --> $config_dir? (y/n)" choice
+		done
+
+		if test $choice == 'y'
+		then
+			cp -r $repo_config_dir $config_dir
+			echo "deployed: $config_name --> $config_dir? (y/n)"
+		fi
+	fi
+done
+
+echo $order completed.
